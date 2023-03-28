34de7fbdc7c73b744693f4b98a21effae7ff0c2a
m2lovric <m2lovric@gmail.com>
2023-03-28 17:53:13 +0200

    Initial Commit

---
+++ Paper_Rock_Scissors/Paper_Rock_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Paper_Rock_ScissorsApp.swift
+//  Paper_Rock_Scissors
+//
+//  Created by Matteo Lovrić on 28.03.2023..
+//
+
+import SwiftUI
+
+@main
+struct Paper_Rock_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
